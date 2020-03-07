406d01f1e39194a4243948f5620934d78ee3b068
Alex Shumilov <ashumilov@pivotal.io>
2020-03-07 10:00:40 -0500

    Initial commit.

---
+++ metrics-generator/src/main/java/io/microsamples/monitoring/metricsgenerator/MetricsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.microsamples.monitoring.metricsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsGeneratorApplication.class, args);
+	}
+
+}
---
+++ metrics-generator/src/test/java/io/microsamples/monitoring/metricsgenerator/MetricsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microsamples.monitoring.metricsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
